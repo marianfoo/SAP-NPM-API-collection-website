40ac912ba160fb66bc0327a9e2859121b843e102
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-07-05 15:20:56 +0200

    feat: add markdown package content

--- src/controller/Object.controller.ts
+++ src/controller/Object.controller.ts
@@ -1,6 +1,7 @@
 import BaseController from "./BaseController";
 import Event from "sap/ui/base/Event";
 import Filter from "sap/ui/model/Filter";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace org.openui5.bestofui5.controller
@@ -25,6 +26,11 @@ export default class Object extends BaseController {
 			path: `/packages/${objectIndex}`,
 			model: "data",
 		});
+
+		let oModel = new JSONModel(`https://raw.githubusercontent.com/marianfoo/SAP-NPM-API-collection-data/live-data/data/docs/${objectName}.json`);
+		this.getView().setModel(oModel, "content");
+		const markdownObject = this.getView().byId("treeMarkdownContent");
+		markdownObject.setContent("");
 	}
 
 	public onPressStandardListItemNpmLink(event: Event): void {
@@ -32,5 +38,11 @@ export default class Object extends BaseController {
 		const npmLink: string = event.getSource().getParent().getBindingContext("data").getObject().npmlink;
 		const link = `${npmLink}/v/${versionNpm}`;
 		window.open(link, "_blank");
+	}
+
+	private onTreeSelect(event: Event): void {
+		const content = event.getParameter("listItem").getBindingContext("content").getObject().content;
+		const markdownObject = this.getView().getParent().getParent().getParent().getParent().getParent().getParent().byId("treeMarkdownContent");
+		markdownObject.setContent(content);
 	}
 }
